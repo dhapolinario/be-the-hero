625817ba052de8e2803fb3a605d98e98802f4845
Dayvis Apolinário <dayvis.apolinario@gmail.com>
2020-04-21 19:49:20 -0300

    Modificado link do github para o branch específico

--- frontend/src/Signature.js
+++ frontend/src/Signature.js
@@ -4,7 +4,7 @@ export default function Signature() {
     return (
       <span style={{fontSize: '0.8em'}}>
         Made with ❤ by Dayvis Apolinário
-        <a style={{margin: '0 8px'}} href="https://github.com/dhapolinario">Github</a>
+        <a style={{margin: '0 8px'}} href="https://github.com/dhapolinario/be-the-hero/tree/feature/firebase">Github</a>
         <a href="https://www.linkedin.com/in/dayvis-apolinario/">LinkedIn</a>
       </span>
     )
