0b13e4914cfbfd211df6a49f54a1c86f11d6fe5e
Dayvis Apolinário <dayvis.apolinario@gmail.com>
2020-04-21 20:34:50 -0300

    Atualizada assinatura no html

--- frontend/src/Signature.js
+++ frontend/src/Signature.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Signature() {
     return (
       <span style={{fontSize: '0.8em'}}>
-        Made with ❤ by Dayvis Apolinário
-        <a style={{margin: '0 8px'}} href="https://github.com/dhapolinario/be-the-hero/tree/feature/firebase">Github</a>
+        Made with ❤ by Dayvis Apolinário at <a href="https://rocketseat.com.br/">Rocketseat OmniStack 11.0</a>
+         | <a style={{margin: '0 8px'}} href="https://github.com/dhapolinario/be-the-hero/tree/feature/firebase">Github</a>
         <a href="https://www.linkedin.com/in/dayvis-apolinario/">LinkedIn</a>
       </span>
     )
